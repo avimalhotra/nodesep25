a0f7b26c514612b3da930a6798a8ba489d7a3bb5
Avinash Malhotra <avimalhotra505@gmail.com>
2025-09-27 11:06:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log(`Hello Node JS`);
+// console.log(process.version);
+
+// console.log( __filename );
+// console.log( __dirname );
+
+// console.log( process==globalThis.process );
+
+// require('./modules.js');
+import "./modules.js";
+
+console.log( uname  );
+console.log( id  );
+console.log( city );
---
+++ modules.js
@@ -0,0 +1,5 @@
+global.uname="avi";
+global.id=22;
+global.city="noida";
+
+const pi=3.14;
