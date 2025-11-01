88ddd6edfa23557c7077efd3e3e2cd0e7eb735ba
Avinash Malhotra <avimalhotra505@gmail.com>
2025-11-01 11:15:40 +0530

    body parser and cookie parse

--- src/app.js
+++ src/app.js
@@ -3,22 +3,34 @@ import path from 'node:path';
 import admin from './routes/admin.js';
 import products from './routes/products.js';
 import search from "./controller/search.js";
+import cookieParser from 'cookie-parser';
 
 const app=express();
 const port=process.env.PORT || 8081;
 
-// app.use(express.static(path.resolve('src/public')));
-// app.use(express.static(path.resolve('node_modules/bootstrap/dist')));
+app.use(express.static(path.resolve('src/public')));
+app.use(express.static(path.resolve('node_modules/bootstrap/dist')));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser('secret'));
+
 
 app.use((req,res,next)=>{
      // console.log(`App starts at ${new Date().toLocaleString()}`);
-     console.log( req.url );
      // console.log( req.url );
+     // console.log( req.url );
+     //  console.log(req.cookies);
+     res.cookie("name","avi", {maxAge:86400, httpOnly: true, signed: true});
+     console.log(req.signedCookies);
      next();
 });
 
 /* routing */
 app.get('/',(req,res)=>{
+    
      res.setHeader('Content-Type','text/html');
      res.status(200).send(`<h1>Hello Express</h1>`);
 });
@@ -29,7 +41,7 @@ function auth(req,res,next){
           next();
      }
      else{
-          res.status(200).send('Can\'t Login');
+          res.status(403).send('Can\'t Login');
      }
 }
  
@@ -45,37 +57,21 @@ app.get('/api',(req,res)=>{
 app.get("/send",(req,res)=>{
      res.status(200).send(`Get data`);
 });
-app.post("/send",(req,res)=>{
-     res.status(200).send(`Post data`);
-});
 
-// app.get("/search",(req,res)=>{
-//      if(req.query.used){
-//            res.status(200).send(`Query: ${req.query.query}, Used: ${req.query.used}`);
-//      }
-//      else{
-//            res.status(200).send(`Query: ${req.query.query}, Used: false`);
-//      }
-// });
+app.post("/send",(req,res)=>{
+     console.log( req.body );
+
+     // res.status(200).send(req.body);
+
+     if( req.body.login=="admin" && req.body.password=="1234"){
+          res.status(200).send(`Welcome ${req.body.login}`);
+     }
+     else{
+          res.status(403).send('Forbidden');
+     }
+});
 
 app.get("/search",search);
-
-
-/* app.get("/products",(req,res)=>{
-      res.status(200).send(`Products Page`)
-})
-app.get("/products/:brand",(req,res)=>{
-      console.log( req.params );
-      res.status(200).send(`Brand: ${req.params.brand}`);
-});
-app.get("/products/:brand/:model",(req,res)=>{
-      console.log( req.params );
-      res.status(200).send(`Brand: ${req.params.model}, Model: ${req.params.brand}`);
-});
-app.get("/products/:brand/:model/:varient",(req,res)=>{
-      console.log( req.params );
-      res.status(200).send(`Brand: ${req.params.brand}, Model: ${req.params.model}, Varient: ${req.params.varient},`);
-}); */
 
 
 app.use("/admin",admin);
